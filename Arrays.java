6b36b27c5d50957e8b154dfcb075189aa7411d42
angelcorpas04 <angelcorpas890.alu@stemgranada.com>
2024-01-22 11:04:32 +0100

    commit

--- Arrays.java
+++ Arrays.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Arrays {
     public static void imprimirArray(String[] lista) {
         System.out.println("Aqui tienes tu lista de Strings:");
@@ -8,6 +10,12 @@ public class Arrays {
     public static void imprimirArray(int[] lista) {
         System.out.println("Aqui tienes tu lista de enteros:");
         for (int elemento : lista){
+            System.out.println(elemento);
+        }
+    }
+    public static void imprimirArray(char[] lista) {
+        System.out.println("Aqui tienes tu lista de letras:");
+        for (char elemento : lista){
             System.out.println(elemento);
         }
     }
@@ -38,10 +46,52 @@ public class Arrays {
         // Probamos la lista de numeros
         System.out.println("Imprimo números: ");
         System.out.println(numeros);
-
         imprimirArray(numeros);
+
+        // Probamos la lista de letras
+        System.out.println("Imprimo letras: ");
+        System.out.println(letras);
+        imprimirArray(letras);
+
+        // El tamaño de la lista:
+        System.out.println("La lista tiene " + colores.length + " elementos");
 
         // ARRAYs DINÁMICAS (LISTA)
         // Creación de arrays dinámica
+        ArrayList<String> arrayColores = new ArrayList<String>();
+        ArrayList<String> arrayColores2 = arrayColores;
+        arrayColores.add("Azul");
+        arrayColores.add("Amarillo");
+        arrayColores.add("Rojo");
+        arrayColores.add("Verde");
+        arrayColores.add("Blanco");
+        arrayColores.add("Negro");
+
+        System.out.println("La lista tiene " + arrayColores.size() + " elementos");
+
+        System.out.print("Imprimo el arrayList de colores:");
+        System.out.println(arrayColores);
+        System.out.print("Imprimo el arrayList de colores2:");
+        System.out.println(arrayColores2);
+
+        // Mostrar o recoger un valor del ArrayList:
+        color = arrayColores.get(0);
+        System.out.println("El color que hay en la primera posición es: " + color);
+
+        // Cambiar un valor del ArrayList:
+        arrayColores.set(1, "4m4rill0");
+        System.out.print("Imprimo el arrayList de colores:");
+        System.out.println(arrayColores);
+        System.out.print("Imprimo el arrayList de colores2:");
+        System.out.println(arrayColores2);
+
+        // Eliminar elemento:
+        arrayColores.remove(1);
+        System.out.print("Imprimo el arrayList de colores eliminando un valor:");
+        System.out.println(arrayColores);
+
+        arrayColores.clear();
+        System.out.print("Imprimo el arrayList de colores eliminando todo:");
+        System.out.println(arrayColores);
     }
 }
